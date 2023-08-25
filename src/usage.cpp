3bd88004c7c8a03139467f4e82f952ae774d293f
Russell Lundberg <lundberg.russell@gmail.com>
2023-08-25 15:24:57 +0700

    modified help message in usage.cpp

--- src/usage.cpp
+++ src/usage.cpp
@@ -5,13 +5,15 @@ void ShowUsage() {
 
     std::cout << "Usage: \n"
               << "Options:\n"
-              << "\t-h,--help\tShow this help message\n"
-              << "\t-b,\thand contains a blank character\n"
-              << "\t-B,\thand contains 1 blank characters\n"
-              << "\t-d,\tSearch the dictionary for the provided string or regex\n"
-              << "\t-e,\tadd the next character to the hand\n"
-              << "\t-l,\tdisplay results with as many letters as specified\n"
-              << "\t-x,\tdisplay results filtered by the provided regex\n"
+              << "\t-h          Show this help message and exit\n"
+              << "\t-b          Rack contains 1 blank character\n"
+              << "\t-B          Rack contains 2 blank characters\n"
+              << "\t-e <chars>  Extend the rack with the following characters\n"
+              << "\t-g <chars>  Create words using these characters as a group\n"
+              << "\t-l <int>    Display results with as many letters as specified\n"
+              << "\t-r <chars>  Rack of characters to combine into words\n"
+              << "\t-s <regex>  Search the dictionary for the provided string or regex\n"
+              << "\t-x <regex>  Filter results matching the provided regex\n"
               << "\n";
 
               return;
